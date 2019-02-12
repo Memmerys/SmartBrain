af211a3d097210a5cd1619ac85dd0ed3f9bc25e8
Helle Emmery <emmery94@gmail.com>
2019-02-12 22:06:39 +0100

    FaceFinder

---
+++ src/App.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+import Clarifai from 'clarifai';
+import Particles from 'react-particles-js';
+import Navigation from './components/navigation/Navigation';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Logo from './components/logo/Logo';
+import Rank from './components/rank/Rank';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: 'bc14802d0e914350bcbf7f1c17936ca1'
+});
+
+const particlesOptions = {
+  particles: {
+    numbers: {
+      value: 500,
+      density: {
+        enable: true,
+        value_area: 500,
+      }
+    }
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false,
+    }
+  }
+
+calculateFaceLocation = (data) => {
+  const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+  const image = document.getElementById('inputimage');
+  const width = Number(image.width);
+  const height = Number(image.height);
+  return {
+    leftCol: clarifaiFace.left_col * width,
+    topRow: clarifaiFace.top_row * height,
+    rightCol: width - (clarifaiFace.right_col * width),
+    bottomRow: height - (clarifaiFace.bottom_row * height)
+  }
+}
+
+displayFaceBox = (box) => {
+  this.setState({box: box})
+}
+
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+
+  onBtnSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    .then((response) => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err));
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
+
+  render() {
+   const { isSignedIn, imageUrl, route, box } = this.state;
+    return (
+      <div className="App">
+      <Particles className='particles'
+      params={{particlesOptions}}          
+      />
+  
+  <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+  { route === 'home'
+    ? <div>
+      <Logo />
+      <Rank />
+      <ImageLinkForm 
+        onInputChange={this.onInputChange} onBtnSubmit={this.onBtnSubmit} />
+  
+      <FaceRecognition box={box} imageUrl={imageUrl}/>
+      </div>
+    : (
+      route === 'signin' ?
+      <SignIn onRouteChange={this.onRouteChange} />
+    : <Register onRouteChange={this.onRouteChange} />
+      )
+  }
+  </div>
+    );
+  }
+}
+export default App;
---
+++ src/components/rank/Rank.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Rank = () => {
+    return (
+      <div>
+        <h1 className= 'white f1'>Face Finder</h1>
+        <div className='white f3'>
+          {'Emmery, your current rank is ....'}
+        </div>
+        <div className='white f1'>
+          {'#4'}
+        </div>
+      </div>  
+    );
+}
+
+export default Rank;
